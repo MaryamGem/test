1ac38eba0f1b59e4c5feece2df1f76b5cd6f7a9b
MaryamGem <maryamsarfraz999@gmail.com>
2023-07-20 17:12:47 +0500

    test

---
+++ mrym.js
@@ -0,0 +1,40 @@
+// var custom = document.getElementsByClassName("text");
+// custom.classList.add("mystyle");
+// console.log("custom",custom);
+
+
+///////////////// First method by query Selector
+
+// var element1 = document.querySelector(".text");
+// element1.classList.add("newClass");
+// console.log("element1",element1);
+
+//////////////// Second method by ID
+
+// var element1 = document.getElementById("text"); // single
+
+var element2 = document.querySelector(".div1")
+
+console.log(element2);
+// var element1 = document.getElementsByClassName("text"); // multiple 
+// element1.classList.add("newClass");
+// console.log("element1",element1);
+
+// var boxes = document.querySelector("span");
+// console.log("boxes",boxes);
+
+// Add tag and use append
+// var child = document.getElementById("h1");
+//       element.appendChild(h1,child);
+
+
+
+//////////////// Append Child
+// const para = document.createElement("p");
+// const node = document.createTextNode("This is my content. I'll put it into p tag");
+// para.innerText = "This is my content. I'll put it into p tag"
+// para.appendChild(node);
+// const element = document.getElementById("div1");
+// element.appendChild(para);
+
+
